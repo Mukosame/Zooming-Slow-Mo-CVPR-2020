c47d44d73ae135a3521ddbc73f6adfdc85853cf8
Mukosame <mukosame@gmail.com>
2020-03-10 10:15:33 -0700

    add data related codes

---
+++ codes/data/util.py
@@ -0,0 +1,202 @@
+import os
+import pickle
+import random
+import numpy as np
+import cv2
+
+####################
+# Files & IO
+####################
+
+###################### get image path list ######################
+IMG_EXTENSIONS = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP']
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+
+def _get_paths_from_images(path):
+    '''get image path list from image folder'''
+    assert os.path.isdir(path), '{:s} is not a valid directory'.format(path)
+    images = []
+    for dirpath, _, fnames in sorted(os.walk(path)):
+        for fname in sorted(fnames):
+            if is_image_file(fname):
+                img_path = os.path.join(dirpath, fname)
+                images.append(img_path)
+    assert images, '{:s} has no valid image file'.format(path)
+    return images
+
+
+def _get_paths_from_lmdb(dataroot):
+    '''get image path list from lmdb meta info'''
+    meta_info = pickle.load(open(os.path.join(dataroot, 'meta_info.pkl'), 'rb'))
+    paths = meta_info['keys']
+    sizes = meta_info['resolution']
+    if len(sizes) == 1:
+        sizes = sizes * len(paths)
+    return paths, sizes
+
+
+def get_image_paths(data_type, dataroot):
+    '''get image path list
+    support lmdb or image files'''
+    paths, sizes = None, None
+    if dataroot is not None:
+        if data_type == 'lmdb':
+            paths, sizes = _get_paths_from_lmdb(dataroot)
+        elif data_type == 'img':
+            paths = sorted(_get_paths_from_images(dataroot))
+        else:
+            raise NotImplementedError('data_type [{:s}] is not recognized.'.format(data_type))
+    return paths, sizes
+
+
+###################### read images ######################
+def _read_img_lmdb(env, key, size):
+    '''read image from lmdb with key (w/ and w/o fixed size)
+    size: (C, H, W) tuple'''
+    with env.begin(write=False) as txn:
+        buf = txn.get(key.encode('ascii'))
+    img_flat = np.frombuffer(buf, dtype=np.uint8)
+    C, H, W = size
+    img = img_flat.reshape(H, W, C)
+    return img
+
+
+def read_img(env, path, size=None):
+    '''read image by cv2 or from lmdb
+    return: Numpy float32, HWC, BGR, [0,1]'''
+    if env is None:  # img
+        img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    else:
+        img = _read_img_lmdb(env, path, size)
+    img = img.astype(np.float32) / 255.
+    if img.ndim == 2:
+        img = np.expand_dims(img, axis=2)
+    # some images have 4 channels
+    if img.shape[2] > 3:
+        img = img[:, :, :3]
+    return img
+
+
+####################
+# image processing
+# process on numpy image
+####################
+
+
+def augment(img_list, hflip=True, rot=True):
+    # horizontal flip OR rotate
+    hflip = hflip and random.random() < 0.5
+    vflip = rot and random.random() < 0.5
+    rot90 = rot and random.random() < 0.5
+
+    def _augment(img):
+        if hflip:
+            img = img[:, ::-1, :]
+        if vflip:
+            img = img[::-1, :, :]
+        if rot90:
+            img = img.transpose(1, 0, 2)
+        return img
+
+    return [_augment(img) for img in img_list]
+
+def channel_convert(in_c, tar_type, img_list):
+    # conversion among BGR, gray and y
+    if in_c == 3 and tar_type == 'gray':  # BGR to gray
+        gray_list = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in img_list]
+        return [np.expand_dims(img, axis=2) for img in gray_list]
+    elif in_c == 3 and tar_type == 'y':  # BGR to y
+        y_list = [bgr2ycbcr(img, only_y=True) for img in img_list]
+        return [np.expand_dims(img, axis=2) for img in y_list]
+    elif in_c == 1 and tar_type == 'RGB':  # gray/y to BGR
+        return [cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) for img in img_list]
+    else:
+        return img_list
+
+
+def rgb2ycbcr(img, only_y=True):
+    '''same as matlab rgb2ycbcr
+    only_y: only return Y channel
+    Input:
+        uint8, [0, 255]
+        float, [0, 1]
+    '''
+    in_img_type = img.dtype
+    img.astype(np.float32)
+    if in_img_type != np.uint8:
+        img *= 255.
+    # convert
+    if only_y:
+        rlt = np.dot(img, [65.481, 128.553, 24.966]) / 255.0 + 16.0
+    else:
+        rlt = np.matmul(img, [[65.481, -37.797, 112.0], [128.553, -74.203, -93.786],
+                              [24.966, 112.0, -18.214]]) / 255.0 + [16, 128, 128]
+    if in_img_type == np.uint8:
+        rlt = rlt.round()
+    else:
+        rlt /= 255.
+    return rlt.astype(in_img_type)
+
+
+def bgr2ycbcr(img, only_y=True):
+    '''bgr version of rgb2ycbcr
+    only_y: only return Y channel
+    Input:
+        uint8, [0, 255]
+        float, [0, 1]
+    '''
+    in_img_type = img.dtype
+    img.astype(np.float32)
+    if in_img_type != np.uint8:
+        img *= 255.
+    # convert
+    if only_y:
+        rlt = np.dot(img, [24.966, 128.553, 65.481]) / 255.0 + 16.0
+    else:
+        rlt = np.matmul(img, [[24.966, 112.0, -18.214], [128.553, -74.203, -93.786],
+                              [65.481, -37.797, 112.0]]) / 255.0 + [16, 128, 128]
+    if in_img_type == np.uint8:
+        rlt = rlt.round()
+    else:
+        rlt /= 255.
+    return rlt.astype(in_img_type)
+
+
+def ycbcr2rgb(img):
+    '''same as matlab ycbcr2rgb
+    Input:
+        uint8, [0, 255]
+        float, [0, 1]
+    '''
+    in_img_type = img.dtype
+    img.astype(np.float32)
+    if in_img_type != np.uint8:
+        img *= 255.
+    # convert
+    rlt = np.matmul(img, [[0.00456621, 0.00456621, 0.00456621], [0, -0.00153632, 0.00791071],
+                          [0.00625893, -0.00318811, 0]]) * 255.0 + [-222.921, 135.576, -276.836]
+    if in_img_type == np.uint8:
+        rlt = rlt.round()
+    else:
+        rlt /= 255.
+    return rlt.astype(in_img_type)
+
+
+def modcrop(img_in, scale):
+    # img_in: Numpy, HWC or HW
+    img = np.copy(img_in)
+    if img.ndim == 2:
+        H, W = img.shape
+        H_r, W_r = H % scale, W % scale
+        img = img[:H - H_r, :W - W_r]
+    elif img.ndim == 3:
+        H, W, C = img.shape
+        H_r, W_r = H % scale, W % scale
+        img = img[:H - H_r, :W - W_r, :]
+    else:
+        raise ValueError('Wrong img ndim: [{:d}].'.format(img.ndim))
+    return img
---
+++ codes/data_scripts/create_lmdb_mp.py
@@ -0,0 +1,145 @@
+'''create lmdb files for Vimeo90K-7 frames training dataset (multiprocessing)
+Will read all the images to the memory
+TODO: change it to adpat more memory limits'''
+
+import os,sys
+import os.path as osp
+import glob
+import pickle
+from multiprocessing import Pool
+import numpy as np
+import lmdb
+import cv2
+try:
+    sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
+    import data.util as data_util
+    import utils.util as util
+except ImportError:
+    pass
+
+
+def reading_image_worker(path, key):
+    '''worker for reading images'''
+    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    return (key, img)
+
+def vimeo7():
+    '''create lmdb for the Vimeo90K-7 frames dataset, each image with fixed size
+    GT: [3, 256, 448]
+        Only need the 4th frame currently, e.g., 00001_0001_4
+    LR: [3, 64, 112]
+        With 1st - 7th frames, e.g., 00001_0001_1, ..., 00001_0001_7
+    key:
+        Use the folder and subfolder names, w/o the frame index, e.g., 00001_0001
+    '''
+    #### configurations
+    mode = 'GT'  # GT | LR
+    batch = 3000 # TODO: depending on your mem size
+    if mode == 'GT':
+        img_folder = '/data/datasets/SR/vimeo_septuplet/sequences/train'
+        lmdb_save_path = '/data/datasets/SR/vimeo_septuplet/vimeo7_train_GT.lmdb'
+        txt_file = '/data/datasets/SR/vimeo_septuplet/sep_trainlist.txt'
+        H_dst, W_dst = 256, 448
+    elif mode == 'LR':
+        img_folder = '/data/datasets/SR/vimeo_septuplet/sequences_LR/LR/x4/train'
+        lmdb_save_path = '/data/datasets/SR/vimeo_septuplet/vimeo7_train_LR7.lmdb'
+        txt_file = '/data/datasets/SR/vimeo_septuplet/sep_trainlist.txt'
+        H_dst, W_dst = 64, 112
+    n_thread = 40
+    ########################################################
+    if not lmdb_save_path.endswith('.lmdb'):
+        raise ValueError("lmdb_save_path must end with \'lmdb\'.")
+    #### whether the lmdb file exist
+    if osp.exists(lmdb_save_path):
+        print('Folder [{:s}] already exists. Exit...'.format(lmdb_save_path))
+        sys.exit(1)
+
+    #### read all the image paths to a list
+    print('Reading image path list ...')
+    with open(txt_file) as f:
+        train_l = f.readlines()
+        train_l = [v.strip() for v in train_l]
+    all_img_list = []
+    keys = []
+    for line in train_l:
+        folder = line.split('/')[0]
+        sub_folder = line.split('/')[1]
+        file_l = glob.glob(osp.join(img_folder, folder, sub_folder) + '/*')
+        all_img_list.extend(file_l)
+        for j in range(7):
+            keys.append('{}_{}_{}'.format(folder, sub_folder, j + 1))
+    all_img_list = sorted(all_img_list)
+    keys = sorted(keys)
+    if mode == 'GT': 
+        all_img_list = [v for v in all_img_list if v.endswith('.png')]
+        keys = [v for v in keys]
+    print('Calculating the total size of images...')
+    data_size = sum(os.stat(v).st_size for v in all_img_list)
+
+    #### read all images to memory (multiprocessing)
+    print('Read images with multiprocessing, #thread: {} ...'.format(n_thread))
+    
+    #### create lmdb environment
+    env = lmdb.open(lmdb_save_path, map_size=data_size * 10)
+    txn = env.begin(write=True)  # txn is a Transaction object
+
+    #### write data to lmdb
+    pbar = util.ProgressBar(len(all_img_list))
+
+    i = 0
+    for path, key in zip(all_img_list, keys):
+        pbar.update('Write {}'.format(key))
+        img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        key_byte = key.encode('ascii')
+        H, W, C = img.shape  # fixed shape
+        assert H == H_dst and W == W_dst and C == 3, 'different shape.'
+        txn.put(key_byte, img)
+        i += 1
+        if  i % batch == 1:
+            txn.commit()
+            txn = env.begin(write=True)
+
+    txn.commit()
+    env.close()
+    print('Finish reading and writing {} images.'.format(len(all_img_list)))
+            
+    print('Finish writing lmdb.')
+
+    #### create meta information
+    meta_info = {}
+    if mode == 'GT':
+        meta_info['name'] = 'Vimeo7_train_GT'
+    elif mode == 'LR':
+        meta_info['name'] = 'Vimeo7_train_LR7'
+    meta_info['resolution'] = '{}_{}_{}'.format(3, H_dst, W_dst)
+    key_set = set()
+    for key in keys:
+        a, b, _ = key.split('_')
+        key_set.add('{}_{}'.format(a, b))
+    meta_info['keys'] = key_set
+    pickle.dump(meta_info, open(osp.join(lmdb_save_path, 'Vimeo7_train_keys.pkl'), "wb"))
+    print('Finish creating lmdb meta info.')
+
+def test_lmdb(dataroot, dataset='vimeo7'):
+    env = lmdb.open(dataroot, readonly=True, lock=False, readahead=False, meminit=False)
+    meta_info = pickle.load(open(osp.join(dataroot, 'meta_info.pkl'), "rb"))
+    print('Name: ', meta_info['name'])
+    print('Resolution: ', meta_info['resolution'])
+    print('# keys: ', len(meta_info['keys']))
+    # read one image
+    if dataset == 'vimeo7':
+        key = '00001_0001_4'
+    else:
+        raise NameError('Please check the filename format.')
+    print('Reading {} for test.'.format(key))
+    with env.begin(write=False) as txn:
+        buf = txn.get(key.encode('ascii'))
+    img_flat = np.frombuffer(buf, dtype=np.uint8)
+    C, H, W = [int(s) for s in meta_info['resolution'].split('_')]
+    img = img_flat.reshape(H, W, C)
+    cv2.imwrite('test.png', img)
+
+
+if __name__ == "__main__":
+    vimeo7()
+    test_lmdb('/data/datasets/SR/vimeo_septuplet/vimeo7_train_GT.lmdb', 'vimeo7')
